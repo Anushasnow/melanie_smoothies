4383e2a3c6857ca58dce3e89d4fa660abad401b4
Anushasnow <nallamalaanusha4u@gmail.com>
2024-11-23 08:27:55 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -52,7 +52,7 @@ if Time_to_insert:
 
 
 
-import requests
-fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(fruityvice_response)
-fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+#fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
