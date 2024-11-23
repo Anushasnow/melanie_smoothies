704d8aabcf78d03b6c1e5b5adbbdc20402d84421
Anushasnow <nallamalaanusha4u@gmail.com>
2024-11-23 08:24:55 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -32,13 +32,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
           ingredients_string += fruit_chosen + ' '
-          fruityvice_response = requests.get("https://smoothiefruit.com/api/fruit/watermelon")
+          fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
           fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True) 
     
 #st.write(ingredients_string)
 
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+          my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
 
 #st.write(my_insert_stmt)
@@ -53,6 +53,6 @@ if Time_to_insert:
 
 
 import requests
-fruityvice_response = requests.get("https://smoothiefruit.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(fruityvice_response)
 fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
